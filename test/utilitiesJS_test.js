48789f5ba1a5c2f2cf6cb7b6712af3029b8ebbbf
Sam Chittenden <sbchittenden@gmail.com>
2017-10-02 13:46:43 -0600

    Start of Utilities test file

---
+++ test/utilitiesJS_test.js
@@ -0,0 +1,103 @@
+var assert = chai.assert;
+
+// replace u variable assignment with correct utilities object reference as needed
+var u = utilities;
+
+describe('Utilities functions', function() {
+
+  /*
+  .by(list, n, callback)
+  Write a function that operates similarly to .forEach. Your function should iterate and call the callback parameter for each element or property of a list at the interval specified by the n parameter. It should not call callback on values greater than the list’s number of elements.
+  */
+  describe('.by', function() {
+    var log = function(val, index, list) {
+      console.log(val);
+    };
+
+    utilities.by([1, 2, 3, 4, 5, 6], 2, log); // will output: 2, 4, 6
+    
+  });
+
+  /*
+  .keys(object)
+   */
+  describe('.keys', function() {
+
+  });
+
+  /*
+  .values(object)
+   */
+  describe('.values', function() {
+
+  });
+
+  /*
+  .pairs(object)
+   */
+  describe('.pairs', function() {
+
+  });
+
+  /*
+  .shuffle(array)
+   */
+  describe('.shuffle', function() {
+
+  });
+
+  /*
+  .pluralize(n, word, pluralWord)
+   */
+  describe('.pluralize', function() {
+
+  });
+
+  /*
+  .toDash(str)
+   */
+  describe('.toDash', function() {
+
+  });
+
+  /*
+  .toCamel(str)
+   */
+  describe('.toCamel', function() {
+
+  });
+
+  /*
+  .has(obj, search)
+   */
+  describe('.has', function() {
+
+  });
+
+  /*
+  .pick(obj, keys)
+   */
+  describe('.pick', function() {
+
+  });
+
+  /*
+  .withoutSymbols(input)
+   */
+  describe('.withoutSymbols', function() {
+
+  });
+
+  /*
+  .countWords(input)
+   */
+  describe('.countWords', function() {
+
+  });
+
+
+
+  // ****************************** //
+  // end of utilities functions
+  // ****************************** //
+});
